875d13deba951d18b34dc047b464aff3888005dd
kureungkureung <terry9611@naver.com>
2022-08-15 19:46:12 +0900

    feat: 팔로우 기능 및 예외 상황 테스트 구현

--- src/test/java/com/example/realworld/domain/user/UserTests.java
+++ src/test/java/com/example/realworld/domain/user/UserTests.java
@@ -1,11 +1,16 @@
 package com.example.realworld.domain.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import com.example.realworld.domain.user.exception.DuplicatedFollowingUserException;
 import com.example.realworld.domain.user.model.User;
 import com.example.realworld.domain.user.model.UserAccountInfo;
+import com.example.realworld.web.exception.ErrorCode;
+import java.util.HashSet;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UserTests {
@@ -44,6 +49,66 @@ public class UserTests {
                 () -> assertThat(updatedUser.image()).isEqualTo(newUser.image()),
                 () -> assertThat(updatedUser.email()).isEqualTo(newUser.email())
         );
+    }
+
+    @Test
+    @DisplayName("팔로우 하기")
+    void followingTest() {
+        // given
+        User user = getUser();
+        User followedUser = getAnotherUser();
+
+        // when
+        user.follow(followedUser);
+
+        // then
+        assertThat(user.followingEmails()).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("이미 팔로우한 유저를 또 팔로우할 때 예외 발생")
+    void followingExceptionTest() {
+        // given
+        User user = getUser();
+        User followedUser = getAnotherUser();
+
+        // when
+        user.follow(followedUser);
+
+        // then
+        assertThatThrownBy(() -> user.follow(followedUser))
+                .isInstanceOf(DuplicatedFollowingUserException.class)
+                .hasMessage(ErrorCode.DuplicatedFollowingUser.message());
+    }
+
+    private User getUser() {
+        UserAccountInfo accountInfo = UserAccountInfo.builder()
+                .username("name")
+                .email("abc@naver.com")
+                .password("password")
+                .build();
+
+        return User.builder()
+                .userAccountInfo(accountInfo)
+                .bio("I like an apple")
+                .image("www.naver.com")
+                .followingEmails(new HashSet<>())
+                .build();
+    }
+
+    private User getAnotherUser() {
+        UserAccountInfo newAccountInfo = UserAccountInfo.builder()
+                .username("name2")
+                .email("email@naver.com")
+                .password("password2")
+                .build();
+
+        return User.builder()
+                .userAccountInfo(newAccountInfo)
+                .bio("I like a banana")
+                .image("www.google.com")
+                .followingEmails(new HashSet<>())
+                .build();
     }
 
 }

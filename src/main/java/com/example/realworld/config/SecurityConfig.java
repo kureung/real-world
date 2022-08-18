14f818a499b24c81ba298b4dd3a52a7fa65bcbd9
kureungkureung <terry9611@naver.com>
2022-08-18 23:53:09 +0900

    feat: 시큐리티 필터에 jwt 필터 등록

--- src/main/java/com/example/realworld/config/SecurityConfig.java
+++ src/main/java/com/example/realworld/config/SecurityConfig.java
@@ -1,15 +1,19 @@
 package com.example.realworld.config;
 
+import com.example.realworld.jwt.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.SecurityContextHolderFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,6 +24,7 @@ public class SecurityConfig {
                 .logout().disable()
                 .authorizeHttpRequests().anyRequest().permitAll();
         http
+                .addFilterBefore(jwtFilter, SecurityContextHolderFilter.class)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
